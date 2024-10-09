826b1c4470dd3e4930453fbf59daaf61023152a4
Calebbuffleben <calebbuffleben@gmail.com>
2024-10-09 17:43:22 -0300

    linked list

--- lists/linked-lists.go
+++ lists/linked-lists.go
@@ -1,12 +1,56 @@
 package lists
 
-import "fmt"
+import ( 
+	"fmt"
+	"strings"
+)
 
 type node struct {
 	value int
 	next *node
 }
 
-func linkedLists() {
+type linkedList struct {
+	head *node
+	len int 
+}
 
+func (l *linkedList) add(value int) {
+	newNode := new(node)
+	newNode.value = value
+	newNode.next = nil
+
+	if l.head == nil {
+		l.head = newNode
+	} else {
+		iterator := l.head
+
+		for ; iterator.next != nil; iterator = iterator.next {
+
+		}
+		iterator.next = newNode
+	}
+
+}
+
+func (l *linkedList) remove(value int) {
+
+}
+
+func (l linkedList) get(value int) *node {
+	return nil
+}
+
+func (l *linkedList) String() string {
+	sb := strings.Builder{}
+
+	for iterator := l.head; iterator != nil; iterator = iterator.next {
+		sb.WriteString(fmt.Sprintf("%d", iterator.value))
+	}
+	return sb.String()
+}
+
+func linkedLists() {
+	l := linkedList{}
+	l.add(1)
 }
