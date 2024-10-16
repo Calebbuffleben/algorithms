cf39712939baffb6600b26d4881612f858a04fb2
Calebbuffleben <calebbuffleben@gmail.com>
2024-10-16 18:13:03 -0300

    change add method

--- lists/linked-lists.go
+++ lists/linked-lists.go
@@ -3,45 +3,58 @@ package lists
 import ( 
 	"fmt"
 	"strings"
+	"errors"
 )
 
 type node struct {
-	value int
+	data int
 	next *node
 }
 
 func (n node) String() string {
-	return fmt.Sprintf("%d", n.value)
+	return fmt.Sprintf("%d", n.data)
 }
 
 type linkedList struct {
 	head *node
-	len int 
+	size int 
 }
 
-func (l *linkedList) add(value int) {
-	newNode := new(node)
-	newNode.value = value
-	newNode.next = nil
+// NewLinkedList creates and returns a new empty linked list
+func NewLinkedList() *linkedList {
+    return &linkedList{head: nil, size: 0}
+}
 
-	if l.head == nil {
-		l.head = newNode
-	} else {
-		iterator := l.head
-
-		for ; iterator.next != nil; iterator = iterator.next {
-
-		}
-		iterator.next = newNode
+func (l *linkedList) add(position int, data int) error {
+	if position < 0 || position > l.size {
+		return errors.New("invalid position")
 	}
 
+	newNode := &node{data: data}
+
+	if position == 0 {
+		//Insert at beggining
+		newNode.next = l.head
+		l.head = newNode;
+	} else {
+		// Insert at the specified position
+		current := l.head
+		for i := 0; i < position - 1; i++{
+			current = current.next
+		}
+		newNode.next = current.next
+		current.next = newNode
+	}
+
+	l.size++
+	return nil
 }
 
 func (l *linkedList) remove(value int) {
 	var previous *node
 
 	for current := l.head; current !=nil; current = current.next {
-		if current.value == value {
+		if current.data == value {
 			if l.head == current {
 				l.head = current.next
 			} else {
@@ -55,7 +68,7 @@ func (l *linkedList) remove(value int) {
 
 func (l linkedList) get(value int) *node {
 	for iteractor := l.head; iteractor != nil; iteractor = iteractor.next {
-		if iteractor.value == value {
+		if iteractor.data == value {
 			return iteractor
 		}
 	}
@@ -67,15 +80,13 @@ func (l *linkedList) String() string {
 	sb := strings.Builder{}
 
 	for iterator := l.head; iterator != nil; iterator = iterator.next {
-		sb.WriteString(fmt.Sprintf("%d", iterator.value))
+		sb.WriteString(fmt.Sprintf("%d", iterator.data))
 	}
 	return sb.String()
 }
 
 func linkedLists() {
 	l := linkedList{}
-	l.add(1)
-	l.add(5)
 	fmt.Println(l)
 	fmt.Println(*(l.get(5)))
 	l.remove(3)
